0f57dbf2231067bf123801ed881ae1056ae146f7
Thierry Ruiz <truiz@ecomundo.eu>
2022-11-17 17:44:04 +0100

    dynamic menu SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
-	// Enable React to support React JSX components.
-	integrations: [react()],
+  // Enable React to support React JSX components.
+  integrations: [react()],
+  output: "server",
+  adapter: node({
+    mode: 'standalone'
+  })
 });
